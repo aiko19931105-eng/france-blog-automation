90a1eeb6453de581e0e1f18ac005a0f90c89f4a0
aiko19931105-eng <aiko19931105@gmail.com>
2026-05-08 16:43:19 +0200

    Update blog_auto_post.py

--- blog_auto_post.py
+++ blog_auto_post.py
@@ -17,7 +17,8 @@ from anthropic import Anthropic
 WP_URL       = os.environ["WP_URL"]
 WP_USER      = os.environ["WP_USER"]
 WP_APP_PASS  = os.environ["WP_APP_PASS"]
-UNSPLASH_KEY = os.environ["UNSPLASH_KEY"]   # Unsplash Access Key
+UNSPLASH_KEY = os.environ["UNSPLASH_KEY"]
+UNSPLASH_KEY:      ${{ secrets.UNSPLASH_KEY }}# Unsplash Access Key
 
 client = Anthropic()
 
